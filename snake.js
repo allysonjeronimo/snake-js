c1d2b80516705e90f778b4a218931155b0fea3e8
Allyson Jeronimo <allysonjeronimo@gmail.com>
2020-03-31 12:36:08 -0300

    Refactoring

--- snake.js
+++ snake.js
@@ -11,10 +11,12 @@
 // 11 - Food Constructor (createFood)
 // 12 - Random
 // 13 - Start
-// 14 - Draw on object
-// 15 - Snake Body
-// 15 - GameOver
+// 14 - Draw in object
+// 15 - Snake Body (array)
+// 16 - GrowUp and Move (this)
+// 15 - Game Over (start(), gameover(), interval)
 // 16 - Text
+// 17 - Score
 
 // variables
 
@@ -34,6 +36,7 @@ var currentKey
 
 window.addEventListener('keydown', function (e) {
     currentKey = e.key
+    this.console.log('Current Key: ', currentKey)
 })
 
 var interval
@@ -41,21 +44,32 @@ var interval
 // game objects
 
 var score = 0
+var hiScore = 0
+var gameOver = false
 var snake
 var food
 var background
 
-// var textScore = {
-//     text: 'Score: 0',
-//     color: 'white',
-//     size: '12px',
-//     font: 'Arial',
-//     outline : 'black',
-//     x: 10,
-//     y: 20
-// }
+var textGameOver = createText('red', canvas.width / 2, canvas.height / 2,
+    'Arial', '16px', 'center', 'Game Over')
+
+var textAfterGameOver = createText('white', canvas.width / 2, canvas.height / 2 + blockSize,
+    'Arial', '12px', 'center', 'Press Enter to Start!')
+
+var textScoreAndHiScore = createText('white', canvas.width / 2, canvas.height / 2 + blockSize * 2,
+    'Arial', '12px', 'center')
+
+var textScore = createText('yellow', 10, 20, 'Arial', '12px')
 
 // functions
+function createText(color, x, y, font, size, align = 'start', defaultValue) {
+    return {
+        color, x, y, font, size, align, defaultValue,
+        draw(value = defaultValue) {
+            renderText(this.color, this.x, this.y, this.font, this.size, this.align, value)
+        }
+    }
+}
 
 function random(min, max) {
     min = Math.ceil(min);
@@ -81,7 +95,7 @@ function createFood() {
         color: 'yellow',
         x: random(0, columns) * blockSize,
         y: random(0, rows) * blockSize,
-        width: blockSize, 
+        width: blockSize,
         height: blockSize,
         draw() {
             render(this.color, this.x, this.y, this.width, this.height)
@@ -90,23 +104,42 @@ function createFood() {
 }
 
 function createSnake() {
+
+    function newBlock(color) {
+        return {
+            color: color,
+            x: 0,
+            y: 0,
+            width: blockSize,
+            height: blockSize
+        }
+    }
+
+    const body = [newBlock('red')]
+
     return {
-        head: {
-            color: 'red',
-            x: random(0, columns) * blockSize,
-            y: random(0, rows) * blockSize,
-            width: blockSize, height: blockSize
+        body: body,
+        head: body[0],
+        growUp() {
+            // add a new sprite on tail
+            var last = this.body[this.body.length - 1]
+
+            var newBodyBlock = newBlock('darkred')
+            newBodyBlock.x = last.x
+            newBodyBlock.y = last.y
+
+            this.body.push(newBodyBlock)
         },
-        body: [],
+        move(i, x, y) {
+            if (i < this.body.length) {
+                var prevX = this.body[i].x
+                var prevY = this.body[i].y
+                this.body[i].x = x
+                this.body[i].y = y
+                this.move(i + 1, prevX, prevY)
+            }
+        },
         draw() {
-
-            render(
-                this.head.color, 
-                this.head.x, 
-                this.head.y, 
-                this.head.width, 
-                this.head.height)
-
             this.body.forEach(b => {
                 render(b.color, b.x, b.y, b.width, b.height)
             })
@@ -119,20 +152,12 @@ function render(color, x, y, width, height) {
     context.fillRect(x, y, width, height)
 }
 
-// function drawText(text){
-
-//     context.font = text.size + ' ' + text.font
-
-//     if(text.outline){
-//         context.strokeStyle = text.outline
-//         context.lineWidth = 4
-//         context.strokeText(text.text, text.x, text.y)
-//     }
-
-//     context.font = text.size + ' ' + text.font
-//     context.fillStyle = text.color
-//     context.fillText(text.text, text.x, text.y)
-// }
+function renderText(color, x, y, font, size, align, value) {
+    context.font = size + ' ' + font
+    context.fillStyle = color
+    context.textAlign = align
+    context.fillText(value, x, y)
+}
 
 function start() {
     // init values
@@ -140,53 +165,71 @@ function start() {
     snake = createSnake()
     food = createFood()
     background = createBackground()
+    gameOver = false
 
-    // start loop
+    // start(re) loop
+    if (interval) {
+        console.log('clear interval!')
+        clearInterval(interval)
+    }
+
+
     interval = setInterval(loop, 200)
 }
 
-// function gameover(){
-//     clearInterval(interval)
-// }
-
 function loop() {
 
-    // process input
+    if (!gameOver) {
 
-    if (currentKey == 'ArrowUp') {
-        snake.head.y -= blockSize
+        // process input
+        if (currentKey == 'ArrowUp') {
+            snake.move(0, snake.head.x, snake.head.y - blockSize)
+        }
+        else if (currentKey == 'ArrowRight') {
+            snake.move(0, snake.head.x + blockSize, snake.head.y)
+        }
+        else if (currentKey == 'ArrowDown') {
+            snake.move(0, snake.head.x, snake.head.y + blockSize)
+        }
+        else if (currentKey == 'ArrowLeft') {
+            snake.move(0, snake.head.x - blockSize, snake.head.y)
+        }
+
+        // update objects (logic, collisions, etc)
+
+        if (snake.head.x == food.x && snake.head.y == food.y) {
+            score++
+            snake.growUp()
+            food = createFood()
+            // textScore.text = 'Score: ' + score
+        }
+
+        if (snake.head.x > canvas.width || snake.head.x < 0 || snake.head.y < 0 || snake.head.y > canvas.height) {
+            gameOver = true
+            hiScore = score > hiScore ? score : hiScore
+        }
+
+        // render result
+
+        context.clearRect(0, 0, canvas.width, canvas.height)
+
+        background.draw()
+        snake.draw()
+        food.draw()
+        textScore.draw('Score: ' + score)
     }
-    else if (currentKey == 'ArrowRight') {
-        snake.head.x += blockSize
+    else {
+
+        if (currentKey == 'Enter')
+            start()
+        else {
+            context.clearRect(0, 0, canvas.width, canvas.height)
+            textGameOver.draw()
+            textAfterGameOver.draw('Press Enter to Start!')
+            textScoreAndHiScore.draw('Score: ' + score + " - Hi-Score: " + hiScore)
+        }
+
     }
-    else if (currentKey == 'ArrowDown') {
-        snake.head.y += blockSize
-    }
-    else if (currentKey == 'ArrowLeft') {
-        snake.head.x -= blockSize
-    }
-
-    // update objects (logic, collisions, etc)
-
-    if (snake.head.x == food.x && snake.head.y == food.y) {
-        score++
-        food = createFood()
-        // textScore.text = 'Score: ' + score
-    }
-
-    // if(snake.x > canvas.width || snake.x < 0 || snake.y < 0 || snake.y > canvas.height){
-    //     gameover()
-    // }
-
-    // render result
-
-    context.clearRect(0, 0, canvas.width, canvas.height)
-
-    background.draw()
-    snake.draw()
-    food.draw()
-
-    // drawText(textScore)
 }
 
 start()
